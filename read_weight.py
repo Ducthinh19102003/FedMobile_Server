5263339cb6e1a0c3eb09557a91d731bc528058c7
lechicuong2k3 <105204420+lechicuong2k3@users.noreply.github.com>
2023-07-19 02:10:06 +0700

    add update_weight

--- read_weight.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-weight = np.frombuffer(open('D:\Desktop\server\FedMobile_Server\client_1_weights.bin', 'rb').read(), dtype=np.float64)
-
-print(weight.shape)
-print(f'weight: {weight}')
